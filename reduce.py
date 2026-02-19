8a006ab8d271623f1f3a703a59fcb0ea4f1b7329
Sparakala21 <paraks@rpi.edu>
2026-02-19 12:16:54 -0500

    2d embeddings+communit-viz

--- reduce.py
+++ reduce.py
@@ -2,7 +2,8 @@ import pandas as pd
 from sklearn.decomposition import PCA
 from umap import UMAP
 import os
-def create_embeddings(tissue_name):
+import sys
+def create_embeddings(tissue_name, method='pca'):
     embeddings = pd.read_csv(f"tissue_embeddings/{tissue_name}_embeddings.csv")
     names = embeddings['node'].tolist()
     emb_matrix = embeddings.drop(columns=['node']).to_numpy()
@@ -11,15 +12,24 @@ def create_embeddings(tissue_name):
     pca = PCA(n_components=50, random_state=42)
 
     embeddings_50d = pca.fit_transform(emb_matrix)
+    if method == 'pca':
+        pca = PCA(n_components=2, random_state=42)
 
-    u = UMAP(n_components=2, random_state=42)
+        embeddings_2d = u.fit_transform(embeddings_50d)
+        return names, embeddings_2d
+    elif method == "umap":
 
-    embeddings_2d = u.fit_transform(embeddings_50d)
-    return names, embeddings_2d
 
+        u = UMAP(n_components=2, random_state=42)
+
+        embeddings_2d = u.fit_transform(embeddings_50d)
+        return names, embeddings_2d
+    else:
+        print("Method not found")
 if __name__ == "__main__":
     #create 2d embeddings for all 64d embedding files in tissue_embeddings folder
     folder = "tissue_embeddings"
+    method = sys.argv[1] if len(sys.argv) > 1 else "umap"
     for file in os.listdir(folder):
         if file.endswith("_embeddings.csv"):
             tissue_name = file.replace("_embeddings.csv", "")
